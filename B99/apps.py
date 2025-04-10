16bc6990989de9e537eff9c9761f4171e88aa0b5
leticia lima <leticia.lima09@aluno.ifce.edu.br>
2025-04-10 12:57:09 -0300

    leticia

---
+++ B99/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class B99Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'B99'
--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(rsko+$#l)z4_4-)-4zn8oshbn8g2^@c(7mx*irlt-&7uod6++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'B99'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
